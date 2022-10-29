46fc2701831035dd1b73a08bf6d31741b4735edf
Richard Lee <richard_875@me.com>
2022-10-29 22:36:13 +1100

    Add algorithms

---
+++ index.py
@@ -0,0 +1,9 @@
+from functions.dijkstra import dijkstra_test
+from functions.floyd import floyd_test
+from functions.kruskal import kruskal_test
+from functions.prim import prim_test
+
+# dijkstra_test()
+# floyd_test()
+# kruskal_test()
+# prim_test()
